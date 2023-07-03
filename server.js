e7dddd246df49ed96a4bdabba99e6896a22c01cc
Zach Dubinsky <zdubinsk@travelers.com>
2023-07-03 15:24:09 -0400

    dao method signatures and imports

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require("express");
-//const dao = require("./data_access");
+//Data access objects
+const character_dao = require("dao/character_access");
+const film_dao = require("dao/film_access");
+const planet_dao = require("dao/planet_access");
+const films_planets_dao = require("dao/films_planets");
+const films_characters_dao = require("dao/films_characters");
+
 const app = express();
 
 app.use(express.json());
