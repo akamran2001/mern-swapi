0c8a22d9303ea354e8a9792e2c0b04fdc0462da0
Ahmed Kamran <akamran@colgate.edu>
2023-07-03 16:02:23 -0400

    film api done

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
 //Data access objects
-const film_dao = require("./film_access");
-const character_dao = require("dao/character_access");
-const planet_dao = require("dao/planet_access");
-const films_planets_dao = require("dao/films_planets");
-const films_characters_dao = require("dao/films_characters");
+const film_dao = require("./dao/film_access");
+// const character_dao = require("dao/character_access");
+// const planet_dao = require("dao/planet_access");
+// const films_planets_dao = require("dao/films_planets");
+// const films_characters_dao = require("dao/films_characters");
 
 const app = express();
 
