84fa830019d5d31284daa0e770df43837b4bea55
Ahmed Kamran <akamran@colgate.edu>
2023-07-05 15:47:13 -0400

    Planet and Planets component

--- star-wars/src/App.js
+++ star-wars/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
+import Planets from "./Planets";
 import Planet from "./Planet";
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Planet></Planet>
+        <Planets></Planets>
+        <Planet planet_id={1}></Planet>
       </header>
     </div>
   );
---
+++ star-wars/src/Planets.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+const fetchPlanets = async (url) => {
+  const response = await fetch(url, {
+    method: "GET", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+  });
+  return await response.json();
+};
+
+export default function Planets() {
+  const [planetList, setPlanetList] = useState([]);
+
+  useEffect(() => {
+    fetchPlanets("http://localhost:3000/api/planets").then((planets) => {
+      console.log(planets);
+      setPlanetList(planets);
+    });
+  }, []);
+
+  return (
+    <>
+      <h3>Planets</h3>
+      <ul>
+        {planetList.map((planet) => {
+          return (
+            <li key={planet.id}>
+              <a href={`/planets/${planet.id}`}>{planet.name}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
